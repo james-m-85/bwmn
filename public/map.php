1988171b12fca0b9c7b685c762294c5b908eeb41
James Moutafidis <james.moutafidis.85@gmail.com>
2019-08-31 17:16:55 +1000

    Updated layout of map details page and included the comments section.

--- public/map.php
+++ public/map.php
@@ -13,8 +13,12 @@
     if (isset($_GET["id"]))
     {
         $mapid = htmlspecialchars($_GET["id"]);
+        
         $sql_mapDetails = "SELECT * FROM maps WHERE mapid='" . $mapid . "'";
         $result_mapDetails = mysqli_query($db_connection, $sql_mapDetails);
+        
+        $sql_mapComments = "SELECT * FROM mapcomments WHERE mapid='" . $mapid . "' ORDER BY cid";
+        $result_mapComments = mysqli_query($db_connection, $sql_mapComments);
     }
     
 ?>
@@ -61,41 +65,79 @@
                 			    $link_melee =    $row["mel"];
                 			    $link_obs =      $row["obs"];
                 			    
-                			    echo "<img src='http://www.panschk.de/mappage/", $pic, "' style='height: auto; width: auto; max-height: 350px; max-width: 350px; float: left; margin-right: 20px;' />";
-                			    echo "<div class='news-article-title'>", $mapname, "</div>";
-                			    echo "<div><b>Map ID: </b> #", $mapid, "</div>";
-                			    echo "<br />";
-                			    echo "<div><b>Author: </b> ", $author, "</div>";
-                			    echo "<div><b>Map Size:</b> ", $map_size, "</div>";
-                			    echo "<div><b>Tileset:</b> ", $tileset, "</div>";
-                			    echo "<div><b>Last Updated:</b> ", $last_updated, "</div>";
-                			    echo "<br />";
+                			    echo "<div class='map-details'>";
+                			    echo "   <div class='news-article-title'>", $mapname, "</div>";
+                			    echo "   <div><b>Map ID: </b> #", $mapid, "</div>";
+                			    echo "   <br />";
+                			    echo "   <div><b>Author: </b> ", $author, "</div>";
+                			    echo "   <div><b>Map Size:</b> ", $map_size, "</div>";
+                			    echo "   <div><b>Tileset:</b> ", $tileset, "</div>";
+                			    echo "   <div><b>Last Updated:</b> ", $last_updated, "</div>";
                 			    
-                			    echo "<div>";
                 			    if (!empty($link_melee))
                 			    {
-                			        echo "<button class='site-button' style='font-size: 16px; margin-right: 10px;'><i class='fas fa-file-download fa-fw icon'></i> Download Melee Version</button>";
+                			        echo "<br />";
+                			        echo "<div>";
+                			        echo "   <button class='site-button'><i class='fas fa-file-download fa-fw icon'></i> Download Melee Version</button>";
+                			        echo "</div>";
                 			    }
                 			    
                 			    if (!empty($link_obs))
                 			    {
-                			        echo "<button class='site-button' style='font-size: 16px;'><i class='fas fa-file-download fa-fw icon'></i> Download Observer Version</button>";
+                			        echo "<br />";
+                			        echo "<div>";
+                			        echo "   <button class='site-button'><i class='fas fa-file-download fa-fw icon'></i> Download Observer Version</button>";
+                			        echo "</div>";
                 			    }
-                			    echo "<div style='clear: both;'></div>";
                 			    echo "</div>";
                 			    
-//                 			    foreach ($row as $key => $value)
-//                 			    {
-//                 			        echo "<div style='color: #3399CC;'><b>", $key, ":</b></div>";
-//                 			        echo "<div>", $value, "</div>";
-//                 			    }
+                			    
+                			    echo "<div class='map-image'>";
+                			    echo "   <img src='http://www.panschk.de/mappage/", $pic, "' />";
+                			    echo "</div>";
+                			    
+                			    echo "<div class='clear'></div>";
                 			}
-                                
+                			
                 			?>
                 			
         				</div>
     				</div>
     			</div>
+    			
+    			
+    			
+    			
+    			
+    			<div class="middle-content">
+					<div class='middle-content-section-outer'>
+						<div class='middle-content-section-inner-map-comments'>
+            				
+                			<?php
+                			
+                			$numberOfComments = mysqli_num_rows($result_mapComments);
+                                
+                			echo "<div class='map-comments-title'><i class='fas fa-comments fa-fw icon'></i> Comments (", $numberOfComments, ")</div>";
+                			while ($row = mysqli_fetch_assoc($result_mapComments))
+                			{
+                			    $cuname =    strip_tags($row["cuname"]);
+                			    $ctext =      stripslashes(nl2br($row["ctext"]));
+                			    
+                			    echo "<div>";
+                			    echo "   <div class='map-comment-username'><i class='fas fa-user fa-fw icon'></i> ", $cuname, "</div>";
+                			    echo "   <div class='map-comment-text'>", $ctext, "</div>";
+                			    echo "</div>";
+                			}
+                			
+                			?>
+                			
+        				</div>
+    				</div>
+    			</div>
+    			
+    			
+    			
+    			
     		</div>
     		
     		
